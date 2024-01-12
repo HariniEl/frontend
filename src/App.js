d1357d4b1883a1ceef4f107b72013c2ae9c29d3a
HariniEl <harielango0605@gmail.com>
2024-01-13 00:37:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Signup from './Signup';
+import Login from './Login';
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import Home from './Home';
+import ForgotPassword from './ForgotPassword';
+import Homepage from './Homepage';
+import ResetPassword from './ResetPassword';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/home' element={<Homepage />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/reset-password/:id/:token' element={<ResetPassword />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Homepage.jsx
@@ -0,0 +1,8 @@
+
+export default function Homepage() {
+    return (
+        <div>
+            <h1>Welcome to Login Page</h1>
+        </div>
+    )
+}
